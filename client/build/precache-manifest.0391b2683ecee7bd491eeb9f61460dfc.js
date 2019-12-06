d669b1487d3e89191e63d9ac10c4a4b0a248d9d9
Saurabh Ghosh <saurabh.ghosh83@gmail.com>
2019-12-06 10:00:23 -0500

    removing client build folder from gitignore

---
+++ client/build/precache-manifest.0391b2683ecee7bd491eeb9f61460dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd1c7dd37efe1d60b572251cdea5f7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68f0a5dee52ff1c7fa83",
+    "url": "/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "68f0a5dee52ff1c7fa83",
+    "url": "/static/js/2.6384efca.chunk.js"
+  },
+  {
+    "revision": "f5edd1714b77abcee12e",
+    "url": "/static/js/main.918335df.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  }
+]);
